ff0a32420914cdbbf6094139e212dc062c2e2f32
KhoiHoangDev <khoih318@gmal.com>
2021-06-11 17:03:24 +0700

    update

--- config/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	// header("Content-type: text/html; charset=utf-8");
-	$con = mysqli_connect("localhost","root","","blog") or die("can not connect to MySQL");
-	mysqli_set_charset($con,"UTF-8");
-
-
-
-?>
